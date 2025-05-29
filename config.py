5791a594bcc35211f7d7336aa4d769f7229f7a3a
Eric Kingery <ekingery25@gmail.com>
2025-05-29 12:24:50 -0700

    Add GitHub Actions and updated config for live/paper support

--- config.py
+++ config.py
@@ -1,22 +1,18 @@
+# config.py
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
-ENV = os.getenv('ENV', 'paper')  # default to paper
-
-API_KEYS = {
-    'paper': {
-        'key': os.getenv('PKOZ1KULBUMRJZUPRVJG'),
-        'secret': os.getenv('G0EaYtsDmZwY8RHogivfGHGmA4oKNxT9dQx4aG6K'),
-        'url': 'https://paper-api.alpaca.markets'
-    },
-    'live': {
-        'key': os.getenv('AKEPWP4ARTWCFMDXWKBC'),
-        'secret': os.getenv('QINfX2EjDQlY5HFA5kChON6Yd1gTXK7Ubgr8kelJ'),
-        'url': 'https://api.alpaca.markets'
-    }
-}
 
 def get_api_credentials():
-    return API_KEYS[ENV]
+    env = os.getenv("TRADING_ENV", "PAPER").upper()
 
+    if env == "LIVE":
+        return {
+            "key": os.getenv("AKEPWP4ARTWCFMDXWKBC"),
+            "secret": os.getenv("QINfX2EjDQlY5HFA5kChON6Yd1gTXK7Ubgr8kelJ"),
+            "url": os.getenv("APCA_API_BASE_URL_LIVE", "https://api.alpaca.markets")
+        }
+    else:
+        return {
+            "key": os.getenv("PKOZ1KULBUMRJZUPRVJG"),
+            "secret": os.getenv("G0EaYtsDmZwY8RHogivfGHGmA4oKNxT9dQx4aG6K"),
+            "url": os.getenv("APCA_API_BASE_URL_PAPER", "https://paper-api.alpaca.markets")
+        }
